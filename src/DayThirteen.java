e2c6bdf5f6958e779367ea796c91da3c86550701
Merel Steenbergen <m.a.steenbergen@student.tudelft.nl>
2020-12-13 23:09:48 +0100

    Add template day fourteen and clean up thirteen

--- src/DayThirteen.java
+++ src/DayThirteen.java
@@ -4,61 +4,51 @@ import java.util.*;
 
 public class DayThirteen {
     public static void main(String[] args) throws FileNotFoundException {
+        // Reading data
         File f = new File("src/inputs/day-thirteen.txt");
         Scanner scan = new Scanner(f);
-        List<String> data = new ArrayList<>();
-        while (scan.hasNextLine()) {
-            String line = scan.nextLine();
-            data.add(line);
-        }
+        String line = "";
+        while (scan.hasNextLine()) line = scan.nextLine();
         List<Bus> busses = new ArrayList<>();
-        List<Bus> unsatisfied = new ArrayList<>();
-        String[] times = data.get(1).split(",");
+        String[] times = line.split(",");
         for (int s = 0; s < times.length; s++) {
             if (times[s].equals("x")) continue;
-            else {
-                busses.add(new Bus(Integer.parseInt(times[s]), s));
-                unsatisfied.add(new Bus(Integer.parseInt(times[s]), s));
-            }
+            else busses.add(new Bus(Integer.parseInt(times[s]), s));
         }
-        long t = busses.get(0).id + busses.get(0).index;
-        System.out.println("t start = " + t);
 
+        // Initializing t
+        long t = busses.get(0).id + busses.get(0).index;
         long toIncrement = 1;
-        while (!unsatisfied.isEmpty()) {
+
+        // Finding right t
+        while (!busses.isEmpty()) {
             Bus toRemove = null;
-            for (Bus bus : unsatisfied) {
-                if ((t+bus.index)%bus.id != 0) {
-                    t += toIncrement;
-                } else {
-                    System.out.println(t + " " + toIncrement + " " + bus.id);
+            for (Bus bus : busses) {
+                if ((t+bus.index)%bus.id != 0) t += toIncrement;
+                else {
                     toRemove = bus;
                     toIncrement *= bus.id;
                 }
                 break;
             }
-            if (toRemove != null) {
-                unsatisfied.remove(toRemove);
-            }
+            if (toRemove != null) busses.remove(toRemove);
         }
+
+        // Printing the answer
         System.out.println("TIME = " + t);
     }
-
 }
 
 class Bus {
     int id;
-    long biggestTime;
     int index;
 
     public Bus(int id, int index) {
         this.id = id;
         this.index = index;
-        this.biggestTime = id;
     }
 
     public String toString() {
         return Integer.toString(this.id);
-
     }
 }
